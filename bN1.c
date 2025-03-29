f0a7ba8d92ac1d3b0ebdec4528fc65d8a75e4cae
mariam05k <mariamkrrt@gmail.com>
2025-03-29 14:03:33 +0400

    Create bN1.c

---
+++ bN1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if((num & 1) == 0){
+    printf("The number is even");
+    } else {
+    printf("The number is odd");
+    }
+    return 0;
+}
