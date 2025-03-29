b49713fb2cf5fe1cb857aa2683e731fd724da231
mariam05k <mariamkrrt@gmail.com>
2025-03-29 15:09:52 +0400

    Create bN9.c

---
+++ bN9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num, n;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Enter the index: ");
+    scanf("%d", &n);
+    num &= ~(1 << n);
+    printf("%d", num);
+  return 0;
+}
